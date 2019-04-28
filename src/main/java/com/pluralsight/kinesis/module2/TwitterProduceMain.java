87f9dc6c749033d241538dffc8e2d3641b23262a
Claudia Hill <hillc255@yahoo.com>
2019-04-28 17:41:10 -0400

    Add kinesis connect code

--- src/main/java/com/pluralsight/kinesis/module2/TwitterProduceMain.java
+++ src/main/java/com/pluralsight/kinesis/module2/TwitterProduceMain.java
@@ -1,10 +1,15 @@
 package com.pluralsight.kinesis.module2;
 
-
+import com.amazonaws.services.kinesis.producer.*;
+import com.google.common.collect.Iterables;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 import twitter4j.*;
-import twitter4j.Status;
-import twitter4j.TwitterException;
 import twitter4j.conf.ConfigurationBuilder;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 
 
 public class TwitterProduceMain {
@@ -16,41 +21,82 @@ public class TwitterProduceMain {
 
     //read and write permissions
     private static TwitterStream createTwitterStream(){
-
         ConfigurationBuilder cb = new ConfigurationBuilder();
         cb.setDebugEnabled(true)
                 .setOAuthConsumerKey("a")
-                .setOAuthConsumerSecret
-                        ("b")
+                .setOAuthConsumerSecret("b")
                 .setOAuthAccessToken("c")
                 .setOAuthAccessTokenSecret("d");
-             //   .setJSONStoreEnabled(true);
 
         return new TwitterStreamFactory(cb.build()).getInstance();
     }
 
-
     private static RawStreamListener createListener(){
-        return new TweetsStatusListener();
+        //Kinesis producer created
+        KinesisProducer kinesisProducer = createKinesisProducer();
+        return new TweetsStatusListener(kinesisProducer);
     }
 
-    private static class TweetsStatusListener implements RawStreamListener {
+    //Kinesis configuration
+    private static KinesisProducer createKinesisProducer(){
+        KinesisProducerConfiguration config = new KinesisProducerConfiguration()
+                .setRequestTimeout(6000)
+                .setRecordMaxBufferedTime(15000)
+                .setRegion("us-east-1") ;
+        return new KinesisProducer(config);
+    }
+
+    static class TweetsStatusListener implements RawStreamListener {
+
+        //Kinesis producer - TweetStatusListener is listening for it
+        private KinesisProducer kinesisProducer;
+        private int count;
+
+        public TweetsStatusListener(KinesisProducer kinesisProducer){
+            this.kinesisProducer = kinesisProducer;
+        }
+
         public void onMessage(String tweetJson){
-            try {
+            if (count++ % 5 !=0) return;
+            try{
                 Status status = TwitterObjectFactory.createStatus(tweetJson);
                 //if new tweet
                 if (status.getUser() != null) {
-                    System.out.println(tweetJson);
-                    System.out.println(status.getUser());
-                    System.out.println(status.getText());
-                }
-            } catch (TwitterException e) {
-                e.printStackTrace();
-            }
+                    byte[] tweetBytes = tweetJson.getBytes(StandardCharsets.UTF_8);
+                    String partitionKey = status.getLang();
+                    ListenableFuture<UserRecordResult> f = kinesisProducer.addUserRecord(
+                            "tweets-stream",
+                            partitionKey,
+                            ByteBuffer.wrap(tweetBytes)
+                    );
 
-            }
-            public void onException(Exception e){
+                    Futures.addCallback(f, new FutureCallback<UserRecordResult>() {
+                        @Override
+                        public void onSuccess(UserRecordResult userRecordResult) {
+                        }
+
+                        @Override
+                        public void onFailure(Throwable throwable) {
+                            if (throwable instanceof UserRecordFailedException) {
+                                UserRecordFailedException e =
+                                        (UserRecordFailedException) throwable;
+                                UserRecordResult result = e.getResult();
+
+                                Attempt last = Iterables.getLast(result.getAttempts());
+                                System.err.println(String.format(
+                                        "Put failed - %s",
+                                        last.getErrorMessage()));
+                            }
+
+                        }
+
+                    });
+                }
+            } catch (TwitterException e){
                 e.printStackTrace();
             }
-     }
+        }
+        public void onException(Exception ex){
+            ex.printStackTrace(); }
+    }
 }
